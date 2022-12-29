0ca45e22b22e73134c41449b5cfdb08421037f13
DylanCheong-tech <74398592+DylanCheong-tech@users.noreply.github.com>
2022-12-29 22:03:54 +0800

    Event Owner Creates Account

--- evoting/urls.py
+++ evoting/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-
+    path('eventowner/createaccount', views.EventOwnerCreateAccountView.as_view()),
 ]
--- evoting/views.py
+++ evoting/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
+
+class EventOwnerCreateAccountView(View):
+    def get(self, request):
+        # render the static page
+        return render(request, "eventowner/signup.html", {})
+
+    def post(self, request):
+        print(request.POST['username'])
+        print(request.POST['email'])
+        print(request.POST['password'])
+
+        # redirect to login page if success
+        return render(request, "eventowner/login.html", {})
+
+        
