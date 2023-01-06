0a133a7a2224d82f55e0651e457ddae00fcb633e
ianyen <ix3zjhty@gmail.com>
2023-01-06 17:07:40 +0800

    Update front-end path for Event Owner View Vote Details, new template tags, fix link

--- evoting/templatetags/overview_extra.py
+++ evoting/templatetags/overview_extra.py
@@ -8,3 +8,17 @@ def concat_period(event, columntype):
         return f"{event.startDate} \n to \n {event.endDate}"
     else:
         return f"{event.startTime} \n to \n {event.endTime}"
+
+@register.filter
+def status_abbreviation(event):
+    status = event.status
+    if status == 'PC':
+        return "Pending Confirmation"
+    elif status == 'PB':
+        return "Published"
+    elif status == 'VC':
+        return "Voting Concluded"
+    elif status == 'FR':
+        return "Final Result Ready"
+    elif status == 'RP':
+        return "Result Publish"
