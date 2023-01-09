5563f9b52391b316a075cc1c963afb44d7c2b386
DylanCheong-tech <74398592+DylanCheong-tech@users.noreply.github.com>
2023-01-09 20:56:43 +0800

    Homepage delete alert box implemented
    1. Pop up alert box for delete vote event confirmation implemented
    2. Vote Event Form modified

--- evoting/static/eventowner/js/overview.js
+++ evoting/static/eventowner/js/overview.js
@@ -1,11 +1,23 @@
 // overview.js
 
+// pop out message box 
+function display_pop_out_message_box(event, confirm_fn) {
+    let message_box = document.getElementById("pop_out_message_box");
+    
+    let confirm_btn = document.getElementById("confirm_btn");
+    confirm_btn.addEventListener("click", function () {
+        confirm_fn(event)
+    }, event)
 
-function deleteVoteEvent(event) {
-    event.preventDefault()
-    let confirmation = confirm("Do you want to delete the Vote Event ?");
+    message_box.style.display = "block";
+}
 
-    if (!confirmation) return;
+function hide_pop_out_message_box(){
+    let message_box = document.getElementById("pop_out_message_box");
+    message_box.style.display = "none";
+}
 
+function confirm_delete_event(event){
     event.target.parentElement.submit();
 }
+
