e26f2c07b029996960211118cba6621f394b3f31
DylanCheong-tech <74398592+DylanCheong-tech@users.noreply.github.com>
2023-01-26 12:23:36 +0800

    View Controller for Voter Access Vote Form Implemented

--- evoting/views/voter_views.py
+++ evoting/views/voter_views.py
@@ -4,10 +4,68 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
 
+from ..models import VoteEvent
+from ..models import VoteOption
+from ..models import Voter
+
 class VoterVoteForm(View):
 	def get(self, request):
+		auth_token = request.GET["auth"]
+		error_message = "Something went wrong ... "
 
-		return render(request, "voter/vote_form.html", {})
+		try :
+			# check the voter is exists, if not an exception will be raised 
+			voter = Voter.objects.get(token=auth_token)
+
+			# check if the voter is casted the vote before 
+			if voter.castedVote != "NOT APPLICABLE":
+				error_message = "Voter has been voted, no access allowed !"
+				raise Exception
+
+			# get the vote event which the voter associated 
+			vote_event_id = voter.eventNo_id
+
+			# get the vote event object 
+			vote_event = VoteEvent.objects.get(eventNo=vote_event_id)
+
+			# check if the vote event is in the Published state
+			# voter will not be get access the PC event, in this state, no auth token will be generated for the voters 
+			if (vote_event.status != "PB"):
+				error_message = "Vote Event has been ended !"
+				raise Exception
+
+			vote_options = VoteOption.objects.filter(eventNo_id=vote_event_id)
+
+			# organise the vote event information to be rendered on the page 
+			vote_event_info = {
+				"eventTitle" : vote_event.eventTitle,
+				"eventQuestion" : vote_event.eventQuestion,
+				"voteOptions" : [{"option" : x.voteOption, "encoding" : x.voteEncoding} for x in vote_options]
+			}
+
+			# organise the voter information to be rendered on the page 
+			voter_info = {
+				"name" : voter.name,
+				"token" : voter.token
+			}
+
+			return render(request, "voter/vote_form.html", {"vote_event_info" : vote_event_info, "voter_info" : voter_info})
+
+
+		except Voter.DoesNotExist:
+			print("No Voter Information Founded !")
+			error_message = "Voter Information Not Founded !"
+
+		except VoteEvent.DoesNotExist:
+			print("No Vote Event Information Founded !")
+			error_message = "Vote Event Information Not Founded !"
+
+		except Exception:
+			print("Error occurred !")
+
+		return render(request, "voter/vote_form.html", {"error_message" : error_message})
+
+
 
 class VoterViewFinalResult(View):
 	def get(self, request):
