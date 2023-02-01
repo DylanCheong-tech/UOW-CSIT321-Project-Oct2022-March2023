a6928331d9bf6db88b60388dd2f6f08881d9e1af
DylanCheong-tech <74398592+DylanCheong-tech@users.noreply.github.com>
2023-02-01 11:15:02 +0800

    Update 1. Create Database and User.sql

--- Database_Scripts/1. Create Database and User.sql
+++ Database_Scripts/1. Create Database and User.sql
@@ -1,5 +1,11 @@
 -- This scripts will create the user for Django to access the database resources 
 
+-- For Developer Machines
 CREATE DATABASE evoting;
 GRANT USAGE on *.* to 'evoting_django'@'localhost' IDENTIFIED BY 'django_password';
 GRANT ALL PRIVILEGES ON evoting.* to 'evoting_django'@'localhost';
+
+-- For Deployment Machines
+CREATE DATABASE evoting;
+GRANT USAGE on *.* to 'evoting_django'@'192.168.0.1' IDENTIFIED BY 'django_password';
+GRANT ALL PRIVILEGES ON evoting.* to 'evoting_django'@'192.168.0.1';
